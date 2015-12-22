e6826a134d4a2c9988111ab0fa1585fcd904e40e
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-12-22 04:07:23 +0200

    Refactor client.py and s1.py

--- src/client.py
+++ src/client.py
@@ -1,19 +1,15 @@
-#!/usr/bin/python           # This is client.py file
+#!/usr/bin/python
 import csv
-import socket               # Import socket module
-import time
+import socket
 
 with open('../data/new.csv') as csvread:
-	reader = csv.DictReader(csvread)
-	for row in reader:
-		uId = row['userId']
-		mId = row['movieId']
-		mRate = row['rating']
-		getStr = 'HTTP GET /item/:'+mId+'/user/:'+uId+'/rat/:'+mRate
-		s = socket.socket()         # Create a socket object
-		host = 'localhost' 	  # Get local machine name
-		port = 8080               # Reserve a port for your service.
-		s.connect((host, port))
-		s.send(bytearray(getStr,'UTF-8'))
-		s.close     		 # Close the socket when done
-		#time.sleep(0.3)
+    for row in csv.DictReader(csvread):
+        requeststr = 'HTTP GET /item/:'+row['movieId']+'/user/:'+row['userId']+'/rat/:'+row['rating']
+        print ('requesting ' + requeststr)
+
+        s = socket.socket()
+        s.connect(('localhost', 8080))
+        s.send(bytearray(requeststr,'UTF-8'))
+        s.close
+
+print('Executed all requests.')
--- src/s1.py
+++ src/s1.py
@@ -1,23 +1,19 @@
-#!/usr/bin/python           # This is server.py file
+#!/usr/bin/python
 import redis
-import socket               # Import socket module
+import socket
 import re
 
 r1 = redis.StrictRedis(host='localhost', port=6379, db=0)
-s = socket.socket()         # Create a socket object
-host = 'localhost'          # Get local machine name
-port = 8080                 # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-s.settimeout(10)
-s.listen(5)                 # Now wait for client connection.
+s = socket.socket()
+s.bind(('localhost', 8080))
+s.listen(5)
+print('Socket is ready.')
 
 while True:
-	c, addr = s.accept()     # Establish connection with client.
-	message = re.findall(r'\b\d+\b', str(c.recv(1024),'UTF-8'))
-	itemUser = message[0] + ':' + message[1] + ':' + message[2]
-	print(itemUser)
-	r1.lpush('qiu', itemUser) 
-	#print (itemUser)
-	#print (message[0])
-	#print (message[1])	
-	c.close()                # Close the connection
+    conn, addr = s.accept()
+    params = re.findall(r'\b\d+\b', str(conn.recv(1024),'UTF-8'))
+    action = params[0] + ':' + params[1] + ':' + params[2]
+    print('Got the following action: ' + action)
+
+    r1.lpush('qiu', action) 
+    conn.close()
