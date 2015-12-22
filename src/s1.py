29dc0e3a0527e2232c445be4f8d22461e147d3b1
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-12-22 07:43:07 +0200

    Better variable names

--- src/s1.py
+++ src/s1.py
@@ -12,7 +12,7 @@ print('Socket is ready.')
 while True:
     conn, addr = s.accept()
     params = re.findall(r'\b\d+\b', str(conn.recv(1024),'UTF-8'))
-    action = params[0] + ':' + params[1] + ':' + params[2]
+    action = params[0] + ':' + params[1] + ':' + params[2]  # item:user:rating
     print('Got the following action: ' + action)
 
     r1.lpush('qiu', action) 
--- src/s2.py
+++ src/s2.py
@@ -8,26 +8,29 @@ r3 = redis.StrictRedis(host='localhost', port=6381, db=0)
 r4 = redis.StrictRedis(host='localhost', port=6382, db=0)
 
 while True:
-	#there must be a list named 'qiu' in r1 first 
+	#There must be a list named 'qiu' in r1 first 
 	if r1.llen('qiu') != 0:
 		pair = r1.lpop('qiu').decode("utf-8")
 
-		#Parse item and user names
+		# Parse item and user names
+		# item:user:rating
 		pairReg = re.match(r'(.*):(.*):(.*)', pair, re.M|re.I)
 
 		item = pairReg.group(1)
 		user = pairReg.group(2)
 		rating = pairReg.group(3)
 
-		#Add 'user' to 'item''s set in r2
-		r2.hset(item, user, int(rating))
-		r4.sadd(user, int(item))
+		# Add the user to item's set in r2
+		r2.hset(item, user, rating)
+		
+		# Add the item to user's set in r4
+		r4.sadd(user, item)
 
-		#Check in all items for 'user'
-		members = r4.smembers(user)
-		while len(members) != 0:
-			citem = members.pop().decode("utf-8")
-			if citem != item:
-				itemUser = item + ':' + citem
-				print(itemUser)
-				r3.lpush('qii', itemUser)
+		# Put all item:alsowatcheditem pairs to qii
+		commonItems = r4.smembers(user)
+		while len(commonItems) != 0:
+			commonItem = commonItems.pop().decode("utf-8")
+			if commonItem != item:
+				itemPair = item + ':' + commonItem
+				print(itemPair)
+				r3.lpush('qii', itemPair)
