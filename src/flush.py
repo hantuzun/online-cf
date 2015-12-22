c8c8a6e5005b558f0bd7a836654eed0757e5cf38
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-12-22 06:24:26 +0200

    Refactor flush.py

--- src/flush.py
+++ src/flush.py
@@ -1,14 +1,7 @@
 #! /usr/bin/env python3
 import redis
-import re
-import time
 
-r1 = redis.StrictRedis(host='localhost', port=6379, db=0)
-r2 = redis.StrictRedis(host='localhost', port=6380, db=0)
-r3 = redis.StrictRedis(host='localhost', port=6381, db=0)
-r4 = redis.StrictRedis(host='localhost', port=6382, db=0)
+redisports = [6379, 6380, 6381, 6382]
 
-r1.flushall()
-r2.flushall()
-r3.flushall()
-r4.flushall()
+for port in redisports:
+    redis.StrictRedis(host='localhost', port=port, db=0).flushall()
