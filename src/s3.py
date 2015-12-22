86389c290f305099559db161e745ff11902e1cfb
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-12-22 05:30:08 +0200

    Fix str functions of s3.py

--- src/s3.py
+++ src/s3.py
@@ -10,7 +10,7 @@ r3 = redis.StrictRedis(host='localhost', port=6381, db=0)
 
 while True:
     if r3.llen('qii') != 0:
-        pair = str(r3.lpop('qii'), "utf-8")
+        pair = str(r3.lpop('qii'))
         pairReg = re.match(r'(.*):(.*)', pair, re.M|re.I)
         
         item1 = pairReg.group(1)
@@ -24,20 +24,21 @@ while True:
         keys2 = r2.hkeys(item2)
         unionCount = 0
         for key in keys1:
-            denom1 += int(str(r2.hget(item1, key), 'utf-8'))**2
+            denom1 += int(str(r2.hget(item1, key)))**2
             if r2.hexists(item2, key):
-                numerator += int(str(r2.hget(item1, key), 'utf-8'))*int(str(r2.hget(item2, key),'utf-8'))
+                numerator += int(str(r2.hget(item1, key)))*int(str(r2.hget(item2, key)))
                 unionCount += 1
             else:
-                numerator += int(str(r2.hget(item1, key), 'utf-8'))*3
+                numerator += int(str(r2.hget(item1, key)))*3
                 unionCount += 1
         for key in keys2:
-            denom2 += int(str(r2.hget(item2, key), 'utf-8'))**2
+            denom2 += int(str(r2.hget(item2, key)))**2
             if r2.hexists(item1,key) != 1:
-                numerator += int(str(r2.hget(item2, key),'utf-8'))*3
+                numerator += int(str(r2.hget(item2, key)))*3
                 unionCount += 1
-        missingCount1 = int(str(r2.get('userCount'),'utf-8')) - r2.hlen(item1)
-        missingCount2 = int(str(r2.get('userCount'), 'utf-8')) - r2.hlen(item2)
+
+        missingCount1 = int(str(r2.get('userCount'))) - r2.hlen(item1)
+        missingCount2 = int(str(r2.get('userCount'))) - r2.hlen(item2)
 
         denom1 += missingCount1 * 9
         denom2 += missingCount2 * 9
