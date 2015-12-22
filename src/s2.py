50b127b395fda77a294fa6defdd91c512d9275de
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-12-22 07:29:06 +0200

    Fix string encoding in s2
    ... and remove archaic userCount

--- src/s2.py
+++ src/s2.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 import redis
 import re
-import time
 
 r1 = redis.StrictRedis(host='localhost', port=6379, db=0)
 r2 = redis.StrictRedis(host='localhost', port=6380, db=0)
@@ -11,27 +10,24 @@ r4 = redis.StrictRedis(host='localhost', port=6382, db=0)
 while True:
 	#there must be a list named 'qiu' in r1 first 
 	if r1.llen('qiu') != 0:
-		pair = str(r1.lpop('qiu')).encode("utf-8")
-		
+		pair = r1.lpop('qiu').decode("utf-8")
+
 		#Parse item and user names
 		pairReg = re.match(r'(.*):(.*):(.*)', pair, re.M|re.I)
 
 		item = pairReg.group(1)
 		user = pairReg.group(2)
 		rating = pairReg.group(3)
-		if r2.exists(user) != 1:
-			r2.incr('userCount')
-		#print(item)
-		#print(user)
-		#test purposesbreak
+
 		#Add 'user' to 'item''s set in r2
-		r2.hset(item,user, int(rating))
+		r2.hset(item, user, int(rating))
 		r4.sadd(user, int(item))
+
 		#Check in all items for 'user'
 		members = r4.smembers(user)
 		while len(members) != 0:
-			citem = str(members.pop()).encode("utf-8")
+			citem = members.pop().decode("utf-8")
 			if citem != item:
 				itemUser = item + ':' + citem
 				print(itemUser)
-				r3.lpush('qii', itemUser)		
+				r3.lpush('qii', itemUser)
